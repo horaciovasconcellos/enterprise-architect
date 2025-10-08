ed34a92cdcdee41e9e2aefaef12b76c049bf0dd5
horacio.vasconcellos <horacio.vasconcellos@gmail.com>
2025-10-08 18:14:32 -0300

    chore: adicionar arquivos de configuração Template

--- site/javascripts/mermaid-theme.js
+++
@@ -1,9 +0,0 @@
-// js/mermaid-theme.js
-document.addEventListener("DOMContentLoaded", () => {
-  const setMermaidTheme = () => {
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    mermaid.initialize({ startOnLoad: true, theme: isDark ? "dark" : "default" });
-  };
-
-  setMermaidTheme();
-});
